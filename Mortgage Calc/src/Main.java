543ab8f1d1f6d2fc364e3f5dcce3d838b0d27af4
Sherwin George <89149622+SherwinGeorge@users.noreply.github.com>
2023-06-04 23:39:02 +0530

    Update Main.java

--- Mortgage Calc/src/Main.java
+++ Mortgage Calc/src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double Mortgage,Interest;
-        int Principal,Period;
+        double Mortgage, Interest;
+        int Principal, Period;
         System.out.print("Enter Principal: ");
         Principal = scanner.nextInt();
         System.out.print("Enter Duration of payment(MONTHS): ");
@@ -12,13 +12,13 @@ public class Main {
         System.out.print("Enter the Interest rate: ");
         Interest = scanner.nextFloat();
 
-        double temp1 = Interest+1;
-        double temp2 = Math.pow(temp1,Period);
+        double temp1 = Interest + 1;
+        double temp2 = Math.pow(temp1, Period);
 
-        Mortgage = ((Interest*temp2)/(temp2-1))*Principal;
+        Mortgage = ((Interest * temp2) / (temp2 - 1)) * Principal;
 
         System.out.print("The Mortgage is $");
-        System.out.format("%.2f",Mortgage);
+        System.out.format("%.2f", Mortgage);
 
     }
 }
